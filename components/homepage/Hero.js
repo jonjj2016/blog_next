bf85ef636188ec79a29acd0332a0def5cd14042d
jonjj2016 <jonmartirosyan1988@gmail.com>
2021-09-19 01:42:58 +0800

    first commit

---
+++ components/homepage/Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from 'next/image'
+import classes from './hero.module.css';
+
+const Hero = () => {
+    return (
+        <section className={classes.hero}>
+            <div className={classes.image}>
+                <Image src="/images/site/main.jpg" alt="imagee showing author" width={300} height={400}/>
+            </div>
+            <h1>Hi, I'm Jon</h1>
+            <p>I blog abot WEB Development especially JS libraries and packages.</p>
+        </section>
+    )
+}
+
+export default Hero
