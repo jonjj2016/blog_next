2093170693b36960e68051b0048c93111a58b0b9
jonjj2016 <jonmartirosyan1988@gmail.com>
2021-09-19 02:04:40 +0800

    added navbar to layout

--- components/layout/mainNavigation.js
+++ components/layout/mainNavigation.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import Logo from './logo';
+import Link from 'next/link';
 
 const MainNavigation = () => {
     return (
-        <div>
-            
-        </div>
+        <header>
+            <Link href='/'>
+                <a>
+                    <Logo/>
+                </a>
+            </Link>
+            <nav>
+                <ul>
+                    <li><Link href='/posts'>Posts</Link></li>
+                    <li><Link href='/contact'>Contact</Link></li>
+                </ul>
+            </nav>
+        </header>
     )
 }
 
